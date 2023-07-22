a87df7790f411ac91047e4e970aa6a20f2016393
RAHUL CHANDRAKANT CHAVAN <matalsanthil@gmail.com>
2023-07-22 13:09:12 +0530

    Initial Commit.

---
+++ tpm/src/main/java/com/nymble/tpm/TpmApplication.java
@@ -0,0 +1,13 @@
+package com.nymble.tpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpmApplication.class, args);
+	}
+
+}
---
+++ tpm/src/test/java/com/nymble/tpm/TpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nymble.tpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
